9b4a621387786682122f654beb4ba3bc2ef198e9
Dean Whitlock <55640632+dwhitlockii@users.noreply.github.com>
2025-06-06 23:49:44 -0400

    Add new ranking scrapers and source selector

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,1 +1,10 @@
 """Scraper package for host2me."""
+
+from . import whtop, hostadvice, hostingadvice_com, techradar
+
+__all__ = [
+    "whtop",
+    "hostadvice",
+    "hostingadvice_com",
+    "techradar",
+]
